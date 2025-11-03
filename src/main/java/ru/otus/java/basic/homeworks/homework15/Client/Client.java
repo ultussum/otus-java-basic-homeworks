e918ee49c70c30a2741647cab6bf612c05e28c63
ultussum <dina.alekseeva720@gmail.com>
2025-11-03 20:07:19 +0300

    ДЗ 15 Консольтный сетевой чат. Часть1

---
+++ src/main/java/ru/otus/java/basic/homeworks/homework15/Client/Client.java
@@ -0,0 +1,72 @@
+package ru.otus.java.basic.homeworks.homework15.Client;
+
+import java.io.*;
+import java.net.Socket;
+import java.util.Scanner;
+
+public class Client {
+    private Socket socket;
+    private DataInputStream in;
+    private DataOutputStream out;
+
+    public Client() {
+        Scanner sc = new Scanner(System.in);
+        try {
+            socket = new Socket("localhost", 8080);
+            in = new DataInputStream(socket.getInputStream());
+            out = new DataOutputStream(socket.getOutputStream());
+            new Thread(() -> {
+                try {
+                    while (true) {
+                        String message = in.readUTF();
+                        if (message.startsWith("/")) {
+                            if (message.startsWith("/exitok")) {
+                                break;
+                            }
+                        } else {
+                            System.out.println(message);
+                        }
+                    }
+                } catch (IOException e) {
+                    throw new RuntimeException(e);
+                } finally {
+                    disconnect();
+                }
+            }).start();
+
+            while (true) {
+                String message = sc.nextLine();
+                out.writeUTF(message);
+                if (message.startsWith("/exit")) {
+                    break;
+                }
+            }
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    public void disconnect() {
+        try {
+            if (in != null) {
+                in.close();
+            }
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+        try {
+            if (out != null) {
+                out.close();
+            }
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+        try {
+            if (socket != null) {
+                socket.close();
+            }
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
+}
