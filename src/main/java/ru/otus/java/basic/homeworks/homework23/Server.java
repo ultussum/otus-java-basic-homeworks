942487b8846f614aa1ce0bac7e13730273c3efdf
ultussum <dina.alekseeva720@gmail.com>
2026-01-31 16:40:12 +0300

    ДЗ 23 Добавлена обработка в потоках

--- src/main/java/ru/otus/java/basic/homeworks/homework23/Server.java
+++ src/main/java/ru/otus/java/basic/homeworks/homework23/Server.java
@@ -3,28 +3,30 @@ package ru.otus.java.basic.homeworks.homework23;
 import java.io.IOException;
 import java.net.ServerSocket;
 import java.net.Socket;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
 
 public class Server {
     private int port;
     private Dispatcher dispatcher;
+    private ExecutorService executorService;
 
     public Server(int port) {
         this.port = port;
         this.dispatcher = new Dispatcher();
+        this.executorService = Executors.newFixedThreadPool(2);
     }
 
     public void start() {
         try (ServerSocket serverSocket = new ServerSocket(port)) {
             System.out.println("Сервер запущен на порту: " + port);
-            try (Socket socket = serverSocket.accept()) {
-                byte[] buffer = new byte[8192];
-                int n = socket.getInputStream().read(buffer);
-                if (n < 1) {
-                    return;
+            while (true){
+                try{
+                    Socket socket = serverSocket.accept();
+                    executorService.submit(new ClientHandler(socket, dispatcher));
+                } catch (IOException e) {
+                    System.err.println("Ошибка при приёме подключения: " + e.getMessage());
                 }
-                String rawRequest = new String(buffer, 0, n);
-                Request request = new Request(rawRequest);
-                dispatcher.execute(request, socket.getOutputStream());
             }
         } catch (IOException e) {
             e.printStackTrace();
